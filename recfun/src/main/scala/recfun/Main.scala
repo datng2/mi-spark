18a1cc2ba675b336aab986f96a90ad5cdbc18317
Dat Nguyen <datng2@tetrationanalytics.com>
2019-02-06 23:28:39 -0800

    Added balance parentheses check

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,4 +1,5 @@
 package recfun
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -14,19 +15,22 @@ object Main {
    * Exercise 1
    */
     def pascal(c: Int, r: Int): Int = {
-      if (c < 0 || r < 0)
-        0
-      else if (c >= math.pow(2, r))
-        0
-      else if (c == 0 && r == 0)
-        1
+      if  (c == 0 || r == c) 1
       else pascal(c - 1, r - 1) + pascal(c, r - 1)
     }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      @tailrec def balanced(chars: List[Char], open: Int): Boolean = chars match {
+        case Nil        => open == 0
+        case '('::tail  => balanced(tail, open + 1)
+        case ')'::tail  => open > 0 && balanced(tail, open - 1)
+        case _::tail    => balanced(tail, open)
+      }
+      balanced(chars, 0)
+    }
   
   /**
    * Exercise 3
